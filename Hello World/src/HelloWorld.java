439b176caa4a0cfd8668f3e42d99e281e20e0cba
Gurumoorthy <Laxmi_Gurumoorthy@Dell.com>
2019-01-09 20:35:58 -0600

    Hello World

---
+++ Hello World/src/HelloWorld.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+
+/**
+ * @author Laxmi_Gurumoorthy
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * 
+	 */
+	public HelloWorld() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+System.out.println("Hello world!");
+	}
+
+}
+
+
+
+
---
+++ Hello World/src/Main.java
@@ -0,0 +1,32 @@
+//import java.util.Scanner;
+//
+//public class Main {
+//
+//	public static void main(String[] args) {
+//		System.out.println("Hello World!");
+//    System.out.println("Please type something into the console.");
+//		Scanner reader = new Scanner(System.in);
+//		String input = reader.nextLine();
+//		System.out.println("You typed: " + input);
+//		reader.close();
+//	}
+//
+//}
+
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+    System.out.println("Hi my name is Laxmi , what's yours?");
+    System.out.println("Hi I am bob!");
+		Scanner reader = new Scanner(System.in);
+		String input = reader.nextLine();
+		System.out.println("Thanks" + ' ' + input);
+		reader.close();
+		
+	}
+
+}
