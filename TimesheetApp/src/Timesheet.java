e1e828785d7518c5c2aa362271b693490079dbca
Gurumoorthy <Laxmi_Gurumoorthy@Dell.com>
2019-02-08 16:52:45 -0600

    Timesheet App

---
+++ TimesheetApp/src/Timesheet.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+
+/**
+ * @author Laxmi_Gurumoorthy
+ *
+ */
+import java.util.ArrayList;
+import java.util.List;
+
+public class Timesheet {
+	private List<TimesheetEntry> database;
+	
+	public Timesheet() {
+		database = new ArrayList<>();
+	}
+	
+	public void add(String project, String task) {
+		String ProjectName = project;
+		String taskName = task;
+	}
+		
+}
